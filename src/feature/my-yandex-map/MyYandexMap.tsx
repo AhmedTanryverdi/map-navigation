146a6d8b44bee45f85f265baddeac47b57645a6b
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-07-09 17:37:44 +0300

    visualize path connection between two markers

--- src/feature/my-yandex-map/MyYandexMap.tsx
+++ src/feature/my-yandex-map/MyYandexMap.tsx
@@ -1,14 +1,10 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./styles.scss";
 
-
 let map: any;
+
 export const MyYandexMap: React.FC = (): React.JSX.Element => {
 	const mapRef = useRef<HTMLDivElement>(null);
-
-	const [initialCoords, setInitialCoords] = useState<
-		[number, number] | undefined
-	>(undefined);
 
 	useEffect(() => {
 		//@ts-ignore
@@ -24,28 +20,51 @@ export const MyYandexMap: React.FC = (): React.JSX.Element => {
 						.get(0)
 						?.geometry?.getCoordinates();
 
-					if (Array.isArray(userLocation)) {
-						//@ts-ignore
-						setInitialCoords(userLocation);
-					} else {
-						setInitialCoords([55.75, 37.6]);
-					}
-				} catch (err) {
-					console.error(
-						"Ошибка при определении местоположения:",
-						err
-					);
-				}
-			});
-		}
-	}, []);
+					let coordsToUse = Array.isArray(userLocation)
+						? userLocation
+						: [55.75, 37.6];
 
-	useEffect(() => {
-		if (initialCoords && mapRef.current != null) {
-			//@ts-ignore
-			map = new window.ymaps.Map(mapRef.current, {
-				center: initialCoords,
-				zoom: 9,
+					//@ts-ignore
+					map = new ymaps.Map(mapRef.current, {
+						center: coordsToUse,
+						zoom: 9,
+					});
+
+					const customIconOptions = {
+						iconLayout: "default#image",
+						iconImageHref: "../../../public/icon-map-mark.png",
+						iconImageSize: [32, 32],
+						iconImageOffset: [-16, -32],
+					};
+
+					//@ts-ignore
+					const placemark = new ymaps.Placemark(
+						coordsToUse,
+						{},
+						customIconOptions
+					);
+					map.geoObjects.add(placemark);
+
+					const moscowCoords = [55.75, 37.6];
+
+					//@ts-ignore
+					const multiRoute = new window.ymaps.multiRouter.MultiRoute(
+						{
+							referencePoints: [moscowCoords, coordsToUse],
+						},
+						{}
+					);
+
+					map.geoObjects.add(multiRoute);
+
+					multiRoute.model.events.add("change", () => {
+						map.setBounds(multiRoute.properties.get("boundedBy"), {
+							checkZoomRange: true,
+						});
+					});
+				} catch (err) {
+					console.error("Ошибка при создании карты:", err);
+				}
 			});
 		}
 
@@ -55,6 +74,7 @@ export const MyYandexMap: React.FC = (): React.JSX.Element => {
 				map = null;
 			}
 		};
-	}, [initialCoords]);
+	}, []);
+
 	return <div ref={mapRef} id="map" className="yandex-map"></div>;
 };
