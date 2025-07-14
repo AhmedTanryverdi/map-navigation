39971f539dd113a651502db80f9b96576b564966
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-07-14 18:55:12 +0300

    refactoring

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,17 @@
-import { Home } from "../feature/home/Home";
+import { LocationInputAside } from "../feature/location-input-aside/LocationInputAside";
+import { MyYandexMap } from "../feature/my-yandex-map/MyYandexMap";
 import "./app.scss";
 
 function App() {
 	return (
-		<>
-			<Home />
-		</>
+		<div className="app">
+			<div className="container">
+				<div className="content">
+					<LocationInputAside />
+					<MyYandexMap />
+				</div>
+			</div>
+		</div>
 	);
 }
 
