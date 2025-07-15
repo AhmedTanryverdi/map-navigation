3750679d991a6c00a5fb51bada643eb29ba3f8a2
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-07-15 13:19:11 +0300

    connected store: redux-toolkit

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './app/App';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import store from "./feature/store/index";
+import App from "./app/App";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</StrictMode>
+);
