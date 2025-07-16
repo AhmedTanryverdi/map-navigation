34e986a66bbcdfeabb9cba40858fbac89aee3876
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-07-16 17:29:36 +0300

    the folder has been moved

--- src/feature/utils/types.ts
+++
@@ -1,6 +0,0 @@
-import { useDispatch } from "react-redux";
-import store from "../store/index";
-
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
-export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
