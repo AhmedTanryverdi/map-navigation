f6d2049fda3cf23f5843c9435bbf56a2c78a92fe
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-07-16 17:23:39 +0300

    added a route between two points

--- src/feature/location-input-aside/LocationInputAside.tsx
+++ src/feature/location-input-aside/LocationInputAside.tsx
@@ -1,18 +1,116 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import {
+	RootState,
+	RouteDataType,
+	useAppDispatch,
+} from "../../shared/utils/types";
+import { setRouteData } from "../slices/routeSlice";
+import { map } from "../my-yandex-map/MyYandexMap";
 import "./styles.scss";
 
 export const LocationInputAside: React.FC = (): React.JSX.Element => {
+	const dispatch = useAppDispatch();
+	const routeData = useSelector<RootState, RouteDataType>(
+		(state) => state.routeData
+	);
+
+	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const { name, value } = event.target;
+		dispatch(setRouteData({ [name]: value }));
+	};
+
+	const geoObjectCollectionRef =
+		//@ts-ignore
+		React.useRef<ymaps.GeoObjectCollection | null>(null);
+	const clearOldRoute = () => {
+		if (geoObjectCollectionRef.current !== null) {
+			geoObjectCollectionRef.current.removeAll();
+			map.geoObjects.remove(geoObjectCollectionRef.current);
+		}
+	};
+	const buildRoute = async () => {
+		clearOldRoute();
+
+		//@ts-ignore
+		if (!map || typeof window.ymaps === "undefined") return;
+
+		try {
+			//@ts-ignore
+			const geocoderResultFrom = await window.ymaps.geocode(
+				routeData.fromAddress
+			);
+
+			//@ts-ignore
+			const geocoderResultTo = await window.ymaps.geocode(
+				routeData.toAddress
+			);
+
+			const firstPoint = geocoderResultFrom.geoObjects
+				.get(0)
+				.geometry.getCoordinates();
+
+			const secondPoint = geocoderResultTo.geoObjects
+				.get(0)
+				.geometry.getCoordinates();
+
+			const multiRoute =
+				//@ts-ignore
+				new window.ymaps.multiRouter.MultiRoute(
+					{
+						referencePoints: [firstPoint, secondPoint],
+					},
+					{}
+				);
+
+			// Создаем новую коллекцию и добавляем туда маршрут
+			//@ts-ignore
+			geoObjectCollectionRef.current = new ymaps.GeoObjectCollection();
+			geoObjectCollectionRef.current.add(multiRoute);
+
+			// Добавляем новую коллекцию на карту
+			map.geoObjects.add(geoObjectCollectionRef.current);
+
+			multiRoute.model.events.add("change", () => {
+				map.setBounds(multiRoute.properties.get("boundedBy"), {
+					checkZoomRange: true,
+				});
+			});
+		} catch (error) {
+			console.error("Ошибка при построении маршрута:", error);
+		}
+	};
+
 	return (
 		<div className="locationInputAside">
 			<div className="container">
 				<div className="content">
 					<h2 className="title">Api Yandex Maps</h2>
-					<label htmlFor="from">
-						<input type="text" placeholder="Откуда?" />
-					</label>
-					<label htmlFor="to">
-						<input type="text" placeholder="Куда?" />
-					</label>
+					<form onSubmit={(event: any) => event.preventDefault()}>
+						<label htmlFor="from">
+							<input
+								type="text"
+								name="fromAddress"
+								id="from"
+								value={routeData.fromAddress}
+								onChange={handleChange}
+								placeholder="Откуда?"
+							/>
+						</label>
+						<label htmlFor="to">
+							<input
+								type="text"
+								name="toAddress"
+								id="to"
+								value={routeData.toAddress}
+								onChange={handleChange}
+								placeholder="Куда?"
+							/>
+						</label>
+						<button type="submit" onClick={() => buildRoute()}>
+							Построить маршрут
+						</button>
+					</form>
 				</div>
 			</div>
 		</div>
